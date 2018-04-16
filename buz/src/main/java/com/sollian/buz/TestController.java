14eb2ee86d27ed54588961fba520982664bcb591
李守宪 <lishouxian@yidian-inc.com>
2018-04-16 16:09:12 +0800

    feat: 实现自定义规则的实时lint检测

---
+++ buz/src/main/java/com/sollian/buz/TestController.java
@@ -0,0 +1,13 @@
+package com.sollian.buz;
+
+import com.sollian.base.Util;
+
+/**
+ * @author sollian on 2018/4/16.
+ */
+
+public class TestController {
+    public void Test() {
+        Util util = new Util();
+    }
+}
